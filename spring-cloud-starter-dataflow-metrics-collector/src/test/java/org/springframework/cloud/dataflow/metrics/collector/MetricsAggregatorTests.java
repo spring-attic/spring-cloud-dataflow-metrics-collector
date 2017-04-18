1f881ae4e463742f425127f7d03f47cecd42b37f
Vinicius Carvalho <viniciusccarvalho@gmail.com>
2017-04-18 09:51:09 -0400

    Fixes #33

--- spring-cloud-starter-dataflow-metrics-collector/src/test/java/org/springframework/cloud/dataflow/metrics/collector/MetricsAggregatorTests.java
+++ spring-cloud-starter-dataflow-metrics-collector/src/test/java/org/springframework/cloud/dataflow/metrics/collector/MetricsAggregatorTests.java
@@ -23,6 +23,7 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
